c57e358ce28bc2b6adbeb6063eda134c2fb341d5
Laura.Raut <laura.rautjoki@gmail.com>
2022-08-02 13:18:08 +0300

    Full Stack open 2022 1.1-1.5

---
+++ src/App.js
@@ -0,0 +1,65 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+
+      <Header course= {course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts}  />
+    </div>
+  )
+}
+  
+  const Header = ({course}) => {
+    return (
+      <div>
+      <h1>{course}</h1>
+      </div>
+   
+   )
+  }
+  const Part = (props) => {
+    return (
+      <div>
+      <p> {props.name} {props.exercises} </p>
+      </div>
+    )
+  }
+
+  const Content = ({parts}) => {
+    return (
+      <div>
+        <Part name={parts[0].name} exercises={parts[0].exercises} />
+        <Part name={parts[1].name} exercises={parts[1].exercises} />
+        <Part name={parts[2].name} exercises={parts[2].exercises} />
+      </div>
+    )
+  }
+
+  const Total = (props) => {
+    return (
+      <div>
+        <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} </p>
+      </div>
+    )
+  }
+
+
+
+export default App
